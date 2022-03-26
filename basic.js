92ae53aacbe20778ff49e1440d6beb0925e1601c
Ezabulbary <ezabul.bary@gmail.com>
2022-03-26 14:58:39 +0600

    core concept

--- basic.js
+++ basic.js
@@ -20,5 +20,39 @@ else if(money == 0){
 
 }
 else if(money != 0){
-    
+
 }
+
+// array 
+
+const numbers = [1, 2, 12, 43, 3, 5, 65, 65, 32];
+const noumberCount = numbers.length;
+numbers.pop();
+numbers.push(111);
+numbers[3] = 89;
+
+if(numbers.indexOf(88) != -1){
+
+}
+if(numbers.includes(88)){
+
+}
+
+// loop
+// while, for
+for(const number of numbers){
+
+}
+
+// function 
+
+function fullName(first, second){
+    const name = first + ' ' + second;
+    return name;
+}
+
+const parsone = fullName('hasan', 'ali');
+
+// object 
+
+const mobile = {color:'black', price: 4000, camera:'have',}
