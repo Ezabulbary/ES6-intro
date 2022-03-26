678fbe9145dcb228d6566c346dfcebdb4ad73c0e
Ezabulbary <ezabul.bary@gmail.com>
2022-03-26 14:30:56 +0600

    js recap

---
+++ basic.js
@@ -0,0 +1,24 @@
+// variable 
+
+var money = 400;
+
+// condition 
+
+if(money > 0){
+
+}
+else if(money < 0){
+
+}
+else if(money >= 0){
+
+}
+else if(money <= 0){
+
+}
+else if(money == 0){
+
+}
+else if(money != 0){
+    
+}
