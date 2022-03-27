71f4c1ea6ab2aed6c01f127167bfb9b50bc14146
Ezabulbary <ezabul.bary@gmail.com>
2022-03-27 10:55:50 +0600

    string templet

---
+++ templet-string.js
@@ -0,0 +1,20 @@
+const karagar = 'ei karagar vangafelo';
+const priya = "opiya opiya tumi kothay";
+const tumi = `tumi jaio na cole`;
+
+const multiline = `first line 
+second line
+third line
+forth line`;
+
+console.log(multiline);
+
+
+const friend = ['abul', 'baule', 'cabule', 'kabule'];
+const count = 5;
+const old = '<h3 class="friend-name">New Friend</h3>';
+const old2 = '<h3 class="friend-name">Friend-' + count + '</h3>';
+const new1 = `<h3 class="friend-name">Friend-${count}</h3>`;
+const new2 = `<h3 class="friend-name">Friend-${friend.length}</h3>`;
+
+console.log(new2);
